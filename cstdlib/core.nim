ea2d165f7639d2c46585ad86bae51fb625dd83ed
Joseph Jaoudi <jjaoudi@google.com>
2019-02-06 19:55:26 -0800

    Update comments in core.nim

--- cstdlib/core.nim
+++ cstdlib/core.nim
@@ -1,9 +1,3 @@
-## All modules must import and export this module.
-##
-## TODO: Explain why.
-
 type
   C* = object
-    ## The root type used to access all C variables/functions/defines.
-    ##
-    ## TODO: Provide examples to show why this type exists.
+    ## The "namespace type" used to access C variables, functions, and defines.
